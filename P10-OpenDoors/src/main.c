6980b538c5214caef48cf68fe50d9c86862b667a
EzeCombina <ezequielcombina1rgmail.com>
2024-09-07 00:25:15 -0300

    Tenth Commit (Cambios)

--- P10-OpenDoors/src/main.c
+++ P10-OpenDoors/src/main.c
@@ -8,24 +8,24 @@
 #include <sys/wait.h>
 #include <stdbool.h>
 
-#define Cant_Puertas 3
+#define Cant_Puertas 4
 
-char Nombres_Puertas[Cant_Puertas] = {'A', 'B', 'C'};
-char Lectura;
+char *Nombres_Puertas[Cant_Puertas] = {"A", "B", "C", "Hola"};
+char Lectura[100] = "\0", *Lectura_ptr;
 
 typedef struct {
-    char Nombre;
+    char *Nombre;
     char *Estado;
 }Puertas;
 
 void *printDoor(void *tid)
 {
     Puertas *Puerta_Seleccionada_ptr = (Puertas*) tid;
-    printf("Puerta creada -> %c\n", Puerta_Seleccionada_ptr->Nombre);
+    printf("Puerta creada -> %s\n", Puerta_Seleccionada_ptr->Nombre);
 
     while(1) 
     {
-        if(Lectura == Puerta_Seleccionada_ptr->Nombre) 
+        if(!strcmp(Lectura_ptr, Puerta_Seleccionada_ptr->Nombre)) 
         {
             //printf("Puerta -> %c\n", Puerta_Seleccionada_ptr->Nombre);
             if(Puerta_Seleccionada_ptr->Estado == "Cerrada") 
@@ -39,11 +39,13 @@ void *printDoor(void *tid)
 
             if(Puerta_Seleccionada_ptr->Estado == "Abierta") 
             {
-                printf("Puerta %c abierta, presione %c para cerrarla.\n", Puerta_Seleccionada_ptr->Nombre, Puerta_Seleccionada_ptr->Nombre);
+                printf("Puerta %s abierta, presione %s para cerrarla.\n", Puerta_Seleccionada_ptr->Nombre, Puerta_Seleccionada_ptr->Nombre);
+                Lectura_ptr = "\0"; 
             } 
             else 
             {
-                printf("Puerta %c cerrada, presione %c para abrirla.\n", Puerta_Seleccionada_ptr->Nombre, Puerta_Seleccionada_ptr->Nombre);
+                printf("Puerta %s cerrada, presione %s para abrirla.\n", Puerta_Seleccionada_ptr->Nombre, Puerta_Seleccionada_ptr->Nombre);
+                Lectura_ptr = "\0";
             }
         }
     }
@@ -52,8 +54,10 @@ void *printDoor(void *tid)
 int main()
 {	
 	pthread_t threads[Cant_Puertas];
-	int status, i;
+	int status, i, k, cont;
     Puertas Puerta_Seleccionada[Cant_Puertas];
+
+    cont = 0;
 
     for(i = 0; i < Cant_Puertas; i++)
     {   
@@ -70,6 +74,24 @@ int main()
 
     while(1)
     {
-        scanf("%c", &Lectura);
+        if(Lectura != '\0' || Lectura != NULL)
+        {
+            Lectura_ptr = (char*)Lectura;
+            for(k = 0; k < Cant_Puertas; k++)
+            {
+                if(strcmp(Lectura_ptr, Nombres_Puertas[k]) && strcmp(Lectura_ptr, "\0"))
+                {
+                    cont++;        
+                }
+            }
+            if(cont == Cant_Puertas)
+            {
+                printf("Valor de puerta ingresado incorrecto.\n");
+                cont = 0;
+            }else{cont = 0;}
+        }
+
+        scanf("%s", Lectura);
+        //scanf("%s", Lectura);
     }
 }
